a37bca60e42142b605296b7260b37a257c85a6d7
Anton Stroganov <stroganov.a@gmail.com>
2011-05-02 22:46:45 -0700

    updating for 0.50 - get rid of unused js and views (main/thumbnail images are now swapped live in spree core)

--- lib/spree_dropdown_variants_hooks.rb
+++ lib/spree_dropdown_variants_hooks.rb
@@ -1,7 +1,3 @@
 class DropdownVariantsHooks < Spree::ThemeSupport::HookListener
   replace  :inside_product_cart_form, 'products/dropdown_variants'
-  
-  insert_after :inside_head do
-    "<%= javascript_include_tag 'dropdown_variants' %>"
-  end
 end
--- public/javascripts/dropdown_variants.js
+++ public/javascripts/dropdown_variants.js
@@ -1,15 +1,5 @@
-$(document).ready(function(){
-  
-  $('#product-variants select').live('change', function(e){
-    var $form = $(this).closest('form');
-    $form.find("button[type=submit]").addClass('disabled');
-    var productId = $form.find("input[name=product]").val();
-    $.ajax({
-       type: "POST",
-       url: "/products/selected_variant/"+productId,
-       data: $form.serialize(),
-       dataType: 'script'
-     });
+jQuery(document).ready(function() {
+  $('#product-variants select').live('change', function (event) {
+    show_variant_images(this.value);
   });
-  
 });
